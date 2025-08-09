81b19ba2a60e4eac78cc0526566914c206439ddf
iw <grubendol@posteo.net>
2025-08-09 16:58:53 +0100

    Refactoring pipeline scripts to function

--- pipeline/main.py
+++ pipeline/main.py
@@ -2,9 +2,14 @@ from extract import extract_data, validate_data
 from transform import transform_data
 from load import load_data_to_sqlite
 
-URL = "~/Downloads/world-happiness/2015.csv"
-if __name__ == "__main__":
+
+def run_pipeline():
     df = extract_data(URL)
     df = validate_data(df)
     df = transform_data(df)
     load_data_to_sqlite(df)
+
+
+URL = "~/Downloads/world-happiness/2015.csv"
+if __name__ == "__main__":
+    run_pipeline()
