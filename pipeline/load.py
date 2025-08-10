1cd59a2312fa650085bcfc42664d153b1225f12f
iw <grubendol@posteo.net>
2025-08-10 10:43:13 +0100

    Fixing wrong input to database writer function

--- pipeline/load.py
+++ pipeline/load.py
@@ -1,12 +1,14 @@
 import sqlite3
+from sqlalchemy import create_engine
 import polars as pl
 
 
+# TODO switch to PostgreSQL
 def load_data_to_sqlite(df: pl.DataFrame, db_path: str = "/opt/airflow/db/etl_demo.db"):
     try:
-        conn = sqlite3.connect(db_path)
-        df.write_database("world_happiness", conn, if_table_exists="replace")
-        conn.close()
+        engine = create_engine(f"sqlite:///{db_path}")
+        df.write_database("world_happiness", engine, if_table_exists="replace")
+        engine.dispose()
     except sqlite3.OperationalError as e:
         # TODO add logging
         print(f"Loading error: {e}")
